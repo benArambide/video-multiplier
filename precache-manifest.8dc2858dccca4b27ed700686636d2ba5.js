217d0ba60263a3ff5b192f24ca8b1347f57e8bfe
Josue Arambide <josueaqp92@gmail.com>
2020-06-04 20:13:36 -0500

    Updates

---
+++ precache-manifest.8dc2858dccca4b27ed700686636d2ba5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0114b5a45daea060ac686fde9b5b4a95",
+    "url": "/video-multiplier/index.html"
+  },
+  {
+    "revision": "98bd2fee19f6d991ff8d",
+    "url": "/video-multiplier/static/css/2.4deadad5.chunk.css"
+  },
+  {
+    "revision": "8a32b32549a1e66adc32",
+    "url": "/video-multiplier/static/css/main.f6673330.chunk.css"
+  },
+  {
+    "revision": "98bd2fee19f6d991ff8d",
+    "url": "/video-multiplier/static/js/2.feddf516.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/video-multiplier/static/js/2.feddf516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a32b32549a1e66adc32",
+    "url": "/video-multiplier/static/js/main.cd6ad36b.chunk.js"
+  },
+  {
+    "revision": "c0bb11139d2c72cdaab8",
+    "url": "/video-multiplier/static/js/runtime-main.e54224ec.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.254d36ae1f97fab69b05d8bb41ba9d7c.js"
+  "/video-multiplier/precache-manifest.8dc2858dccca4b27ed700686636d2ba5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/video-multiplier/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
